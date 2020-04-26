8c4372a784056f14cb6c646ab69c18301b57f59f
WilliXL <liuwilliam47@gmail.com>
2020-04-26 16:00:20 -0500

    restarting framework

--- code/Range.h
+++ code/Range.h
@@ -1,9 +1,7 @@
 #ifndef _RANGE_H_
 #define _RANGE_H_
 
-#include "llvm/Support/raw_ostream.h"
-
-// #include "SAGEExpr.h"
+#include <cstdint>
 
 class Range {
 public:
@@ -15,26 +13,26 @@ public:
 	void setLower(const int64_t& Lower) { Lower_ = Lower; }
 	void setUpper(const int64_t& Upper) { Upper_ = Upper; }
 
-    Range operator+(const Range& Other) {
-        return Range(getLower() + Other.getLower(), getUpper() + Other.getUpper());
-    }
-    Range operator-(const Range& Other) {
-        return Range(getLower() - Other.getUpper(), getUpper() - Other.getLower());
-    }
-    Range operator*(const Range& Other) {
-        int64_t LL = getLower() * Other.getLower(),
-                LU = getLower() * Other.getUpper(),
-                UL = getUpper() * Other.getLower(),
-                UU = getUpper() * Other.getUpper();
-        return Range(std::min(LL,std::min(LU,std::min(UL,UU))), std::max(LL,std::max(LU,std::max(UL,UU))));
-    }
-    Range operator/(const Range& Other) {
-        int64_t LL = getLower()/Other.getLower(),
-                LU = getLower()/Other.getUpper(),
-                UL = getUpper()/Other.getLower(),
-                UU = getUpper()/Other.getUpper();
-        return Range(std::min(LL,std::min(LU,std::min(UL,UU))), std::max(LL,std::max(LU,std::max(UL,UU))));
-    }
+    // Range operator+(const Range& Other) {
+    //     return Range(getLower() + Other.getLower(), getUpper() + Other.getUpper());
+    // }
+    // Range operator-(const Range& Other) {
+    //     return Range(getLower() - Other.getUpper(), getUpper() - Other.getLower());
+    // }
+    // Range operator*(const Range& Other) {
+    //     int64_t LL = getLower() * Other.getLower(),
+    //             LU = getLower() * Other.getUpper(),
+    //             UL = getUpper() * Other.getLower(),
+    //             UU = getUpper() * Other.getUpper();
+    //     return Range(std::min(LL,std::min(LU,std::min(UL,UU))), std::max(LL,std::max(LU,std::max(UL,UU))));
+    // }
+    // Range operator/(const Range& Other) {
+    //     int64_t LL = getLower()/Other.getLower(),
+    //             LU = getLower()/Other.getUpper(),
+    //             UL = getUpper()/Other.getLower(),
+    //             UU = getUpper()/Other.getUpper();
+    //     return Range(std::min(LL,std::min(LU,std::min(UL,UU))), std::max(LL,std::max(LU,std::max(UL,UU))));
+    // }
 
 	bool operator==(const Range& Other) {
         return getLower() == Other.getLower() &&
@@ -47,8 +45,7 @@ public:
 private:
 	int64_t Lower_;
 	int64_t Upper_;
+    int32_t Stride_;
 };
-
-// raw_ostream& operator<<(raw_ostream& OS, const Range& R);
 
 #endif
