964978b3c05805226bd1efbf8a28215d7a323bcb
WilliXL <liuwilliam47@gmail.com>
2020-04-26 16:07:58 -0500

    interactivity?

--- code/Range.h
+++ code/Range.h
@@ -15,6 +15,20 @@ public:
 	void setUpper(const int64_t& Upper) { Upper_ = Upper; }
     void setStride(const int32_t& Stride) { Stride_ = Stride; }
 
+    bool interacts(Range& r1, Range& r2) {
+        int64_t r1Lower = r1.getLower(), r2Lower = r2.getLower();
+        if (r1Lower == r2Lower) return true;
+        int32_t s1 = r1.getStride(), s2 = r2.getStride();
+        int64_t i1 = r1Lower+s1, i2 = r2Lower+s2;
+        int64_t r1Upper = r1.getUpper(), r2Upper = r2.getUpper();
+        while (i1 < r1Upper and i2 < r2Upper) {
+            if (i1 == i2) return true;
+            i1 += s1;
+            i2 += s2;
+        }
+        return false;
+    }
+
     // Range operator+(const Range& Other) {
     //     return Range(getLower() + Other.getLower(), getUpper() + Other.getUpper());
     // }
