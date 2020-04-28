17064b9d0317b32b3a01987e70d951b3ffa70425
WilliXL <liuwilliam47@gmail.com>
2020-04-27 23:42:42 -0500

    yeet

--- code/RangeAnalysis.cpp
+++ code/RangeAnalysis.cpp
@@ -4,7 +4,19 @@ void RangeAnalysis::getAnalysisUsage(AnalysisUsage &AU) const {
     AU.addRequired<LoopInfoWrapperPass>();
     AU.addRequired<ScalarEvolutionWrapperPass>();
     AU.setPreservesCFG();
-    // return false;
+}
+
+const SCEV* RangeAnalysis::getIndexVariableVectorized(ScalarEvolution& SE, const StoreInst* SI) {
+    auto bitCast = SI->getPointerOperand();
+    if (auto bitCastInst = dyn_cast<BitCastInst>(bitCast)) {
+        auto ptr = bitCastInst->getOperand(0);
+        if (auto getEltPtrInst = dyn_cast<GetElementPtrInst>(ptr)) {
+            auto indices = getEltPtrInst->indices();
+            auto scev = SE.getSCEV(&(**(indices.begin())));
+            return scev;
+        }
+    }
+    return NULL;
 }
 
 bool RangeAnalysis::runOnLoop(Loop* L, LPPassManager &LPM) {
@@ -12,23 +24,12 @@ bool RangeAnalysis::runOnLoop(Loop* L, LPPassManager &LPM) {
     LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
     auto &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
 
-    auto IV = L->getInductionVariable(SE);
-    outs() << "IV: " << IV << "\n";
-    auto IV_SC = SE.getSCEV(IV);
-    ConstantRange R = SE.getSignedRange(IV_SC);
-    auto upper = SE.getSignedRangeMax(IV_SC);
-    auto lower = SE.getSignedRangeMin(IV_SC);
-    outs() << "Upper: " << upper << ", Lower: " << lower << "\n";
-    outs() << *IV_SC << "\n";
-
     std::vector<const SCEV*> gep_scev;
     for (BasicBlock *BB: L->getBlocks()) {
         for (Instruction &I: *BB) {
-            if (auto si = dyn_cast<GetElementPtrInst>(&I)) {
-                for (auto it = si->idx_begin(); it != si->idx_end(); ++it) {
-                    auto SCEV = SE.getSCEV(*it);
+            if (auto si = dyn_cast<StoreInst >(&I)) {
+                if (auto SCEV = getIndexVariableVectorized(SE,si)) {
                     gep_scev.push_back(SCEV);
-                    outs() << *SCEV << "\n";
                 }
             }
         }
@@ -38,6 +39,7 @@ bool RangeAnalysis::runOnLoop(Loop* L, LPPassManager &LPM) {
         unsigned minByteDiff = UINT_MAX;
         for (int j=0;j<gep_scev.size();j++) {
             if (i == j) continue;
+            outs() << i << "," << j << "\n";
             const SCEV *AS = gep_scev[i], *BS = gep_scev[j];
             unsigned bitWidth = SE.getTypeSizeInBits(AS->getType());
             auto diff = SE.getMinusSCEV(AS, BS);
