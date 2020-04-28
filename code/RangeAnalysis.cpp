b8482cfa281b0afb5f10444a53916b24b4381526
Weihang Fan <weihangf@gmail.com>
2020-04-28 00:42:09 -0400

    cleanup

--- code/RangeAnalysis.cpp
+++ code/RangeAnalysis.cpp
@@ -12,39 +12,32 @@ bool RangeAnalysis::runOnLoop(Loop* L, LPPassManager &LPM) {
     LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
     auto &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
 
-    auto IV = L->getInductionVariable(SE);
-    outs() << "IV: " << IV << "\n";
-    auto IV_SC = SE.getSCEV(IV);
-    ConstantRange R = SE.getSignedRange(IV_SC);
-    auto upper = SE.getSignedRangeMax(IV_SC);
-    auto lower = SE.getSignedRangeMin(IV_SC);
-    outs() << "Upper: " << upper << ", Lower: " << lower << "\n";
-    outs() << *IV_SC << "\n";
-
-    std::vector<const SCEV*> gep_scev;
+    std::vector<Value*> gep_scev;
     for (BasicBlock *BB: L->getBlocks()) {
         for (Instruction &I: *BB) {
             if (auto si = dyn_cast<GetElementPtrInst>(&I)) {
                 for (auto it = si->idx_begin(); it != si->idx_end(); ++it) {
                     auto SCEV = SE.getSCEV(*it);
-                    gep_scev.push_back(SCEV);
+                    gep_scev.push_back(*it);
                     outs() << *SCEV << "\n";
                 }
             }
         }
     }
 
+    std::map<Value*, bool> m;
     for (int i=0;i<gep_scev.size();i++) {
         unsigned minByteDiff = UINT_MAX;
         for (int j=0;j<gep_scev.size();j++) {
             if (i == j) continue;
-            const SCEV *AS = gep_scev[i], *BS = gep_scev[j];
+            const SCEV *AS = SE.getSCEV(gep_scev[i]), *BS = SE.getSCEV(gep_scev[j]);
             unsigned bitWidth = SE.getTypeSizeInBits(AS->getType());
             auto diff = SE.getMinusSCEV(AS, BS);
             outs() << "diff range: " << SE.getUnsignedRangeMin(diff) << " " << SE.getUnsignedRangeMax(diff) << "\n";
-            unsigned byteDiff = bitWidth * SE.getUnsignedRangeMin(diff).getLimitedValue() / 8;
+            unsigned byteDiff = bitWidth * SE.getUnsignedRangeMin(diff).abs().getLimitedValue() / 8;
             minByteDiff = std::min(minByteDiff, byteDiff);
         }
+        m[gep_scev[i]] = minByteDiff < 64;
         outs() << "min byte diff: " << minByteDiff << "\n";
     }
 
