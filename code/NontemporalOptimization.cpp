43bef7b8c6945eced719625fcb081393d169085f
Weihang Fan <weihangf@gmail.com>
2020-05-02 23:33:28 -0400

    compiles

--- code/NontemporalOptimization.cpp
+++ code/NontemporalOptimization.cpp
@@ -15,19 +15,19 @@ void AccessAnalysis::getAnalysisUsage(AnalysisUsage &AU) const {
 
 bool AccessAnalysis::doInitialization(Function& F) {
     int idx = 0;
-    for (auto BB : F) {
-        for (auto I : BB) {
+    for (auto& BB : F) {
+        for (Instruction& I : BB) {
             if (isa<StoreInst>(I) || isa<LoadInst>(I)) {
-                InstsToIdx_[I] = idx;
+                InstsToIdx_[&I] = idx;
                 idx++;
-                InstVector_.push_back(I);
+                InstVector_.push_back(&I);
             }
         }
     }
     return false;
 }
 
-void AccessAnalysis::doDfaAnalysis(void) {
+std::pair<std::map<BasicBlock*, BitVector>, std::map<BasicBlock*, BitVector>> AccessAnalysis::doDfaAnalysis(Function &F) {
     BitVector::size_type size = InstsToIdx_.size();
     BitVector boundary = BitVector(size);
     BitVector init = BitVector(size, true);
@@ -35,8 +35,8 @@ void AccessAnalysis::doDfaAnalysis(void) {
     DataflowAnalysis DFAAccess(
         size, // size
         false, // backward
-        [&InstsToIdx_](Instruction* I, BitVector BV) {
-            return BV.set(InstsToIdx_[I]);
+        [this](Instruction* I, BitVector BV) {
+            return BV.set(this->InstsToIdx_[I]);
         }, // transfer func (just propagate this instruction up)
         [](BasicBlock*, BasicBlock*, BitVector& in1, BitVector& in2) {
             in1 |= in2; // meet operator is bitwise OR (union)
@@ -55,12 +55,12 @@ bool AccessAnalysis::ptsToSameStruct(Instruction* instOne, Instruction* instTwo)
     if (StoreInst* SITwo = dyn_cast<StoreInst>(instTwo)) {
         GetElementPtrInst* GEPITwo = dyn_cast<GetElementPtrInst>(SITwo->getPointerOperand());
         auto DSTwo = GEPITwo->getPointerOperand();
-        return DSOne == DSTwo
+        return DSOne == DSTwo;
     }
     if (LoadInst* LITwo = dyn_cast<LoadInst>(instTwo)) {
-        GetElementPtrInst* GEPITwo = dyn_cast<GetElementPtrInst>(SITwo->getPointerOperand());
+        GetElementPtrInst* GEPITwo = dyn_cast<GetElementPtrInst>(LITwo->getPointerOperand());
         auto DSTwo = GEPITwo->getPointerOperand();
-        return DSOne == DSTwo
+        return DSOne == DSTwo;
     }
     return false;
 
@@ -69,62 +69,103 @@ bool AccessAnalysis::ptsToSameStruct(Instruction* instOne, Instruction* instTwo)
 
 void AccessAnalysis::insertNontemporalInstruction(Instruction* inst) {
     StoreInst* SI = dyn_cast<StoreInst>(inst);
-    auto MDN = MDNode::get(BB->getContext(), MDString::get(BB->getContext(), to_string(1)));
-    SI.setMetadata("nontemporal", MDN);
+    auto MDN = MDNode::get(inst->getParent()->getContext(), MDString::get(inst->getParent()->getContext(), std::to_string(1)));
+    SI->setMetadata("nontemporal", MDN);
 }
 
-int AccessAnalysis::getReuseDistance(Instruction* instOne, Instruction* instTwo) {
+unsigned AccessAnalysis::getLoopCacheFootprint(Loop* L, LoopInfo &LI, ScalarEvolution &SE) {
+    unsigned total = 0;
+    for (auto BB : L->getBlocks()) {
+        for (Instruction& I: *BB) {
+            if (isa<StoreInst>(I) || isa<LoadInst>(I)) {
+                IndexedReference IR(I, LI, SE);
+                total += IR.computeRefCost(*L, 64);
+            }
+        }
+    }
+    return total;
+}
 
+int AccessAnalysis::getReuseDistance(Instruction* instOne, Instruction* instTwo, LoopInfo &LI, ScalarEvolution &SE, DependenceInfo &DI, AliasAnalysis &AA) {
     BasicBlock* BBOne = instOne->getParent();
     BasicBlock* BBTwo = instTwo->getParent();
-    const Loop* LoopOne = LI[BBOne];
-    const Loop* LoopTwo = LI[BBTwo];
-    auto IROne = IndexedReference(instOne, LI, SE); // TODO member variables???
-    auto IRTwo = IndexedReference(instTwo, LI, SE);
+    Loop* LoopOne = LI.getLoopFor(BBOne);
+    Loop* LoopTwo = LI.getLoopFor(BBTwo);
+    auto IROne = IndexedReference(*instOne, LI, SE); // TODO member variables???
+    auto IRTwo = IndexedReference(*instTwo, LI, SE);
 
     if (LoopOne == LoopTwo) {
         auto hasTempReuse = IROne.hasTemporalReuse(IRTwo,MIN_REUSE_DIST,*LoopOne,DI,AA);
         if (hasTempReuse.hasValue()) {
-            if (hasTempReuse.value()) {
+            if (hasTempReuse.getValue()) {
                 return MIN_REUSE_DIST - 1;
             }
             else {
                 return MIN_REUSE_DIST;
             }
+        } else {
+            // Dont care
+            return 0;
         }
     }
-    else if (LoopOne.getParentLoop() == LoopTwo || LoopTwo.getParentLoop() == LoopOne) {
+    else if (LI.getLoopDepth(BBOne) != LI.getLoopDepth(BBTwo)) {
         return 0; // let's not deal with this for now
     }
-    else if ()
-
-    return 0;
+    else {
+        Loop *LOne = LoopOne, *LTwo = LoopTwo;
+        while (LOne->getParentLoop() != LTwo->getParentLoop()) {
+            LOne = LOne->getParentLoop();
+            LTwo = LTwo->getParentLoop();
+        }
+        unsigned totalReuseDistance = 0;
+        if (LOne->getParentLoop() == nullptr) {
+            bool on = false;
+            for (auto it = LI.begin(); it != LI.end(); ++it) {
+                if ((*it)->contains(BBOne)) on = true;
+                if ((*it)->contains(BBTwo)) break;
+                if (on) {
+                    totalReuseDistance += getLoopCacheFootprint(*it, LI, SE);
+                }
+            }
+        } else {
+            bool on = false;
+            const std::vector<Loop*> subLoops = LOne->getSubLoops();
+            for (auto it = subLoops.begin(); it != subLoops.end(); ++it) {
+                if ((*it)->contains(BBOne)) on = true;
+                if ((*it)->contains(BBTwo)) break;
+                if (on) {
+                    totalReuseDistance += getLoopCacheFootprint(*it, LI, SE);
+                }
+            }
+        }
+        return totalReuseDistance;
+    }
 }
 
-void AccessAnalysis::insertNontemporalInsts() {
-    for (auto it = InstToIdx_.begin(); it != InstToIdx_.end(); ++it) {
+void AccessAnalysis::insertNontemporalInsts(LoopInfo &LI, ScalarEvolution &SE, DependenceInfo &DI, AliasAnalysis &AA) {
+    for (auto it = InstsToIdx_.begin(); it != InstsToIdx_.end(); ++it) {
         auto instOne = it->first;
         if (!isa<StoreInst>(instOne)) continue;
-        auto parentBlock = key->getParent();
+        auto parentBlock = instOne->getParent();
         auto BV = BBOuts_[parentBlock];
         for (auto idx : BV.set_bits()) {
             auto instTwo = InstVector_[idx];
             if (!ptsToSameStruct(instOne,instTwo)) continue;
-            auto RD = getReuseDistance(instOne,instTwo);
+            auto RD = getReuseDistance(instOne,instTwo, LI, SE, DI, AA);
             if (RD >= MIN_REUSE_DIST) insertNontemporalInstruction(instOne);
         }
     }
 }
 
 bool AccessAnalysis::runOnFunction(Function& F) {
-    LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+    auto &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
     auto &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
     auto &DI = getAnalysis<DependenceAnalysisWrapperPass>().getDI();
     auto &AA = getAnalysis<AAResultsWrapperPass>().getAAResults();
 
-    auto accessInOut = doDfaAnalysis();
-    BBOuts_ = accessInOut.second();
-    insertNontemporalInsts();
+    auto accessInOut = doDfaAnalysis(F);
+    BBOuts_ = accessInOut.second;
+    insertNontemporalInsts(LI, SE, DI, AA);
 
     return true;
 }
